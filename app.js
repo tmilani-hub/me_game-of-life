7c7b611649797e0c0f43f46c63fe7a3f1a013000
tmilani-hub <tmilani@edenschool.fr>
2026-04-20 17:36:25 +0200

    msg

--- app.js
+++ app.js
@@ -4,43 +4,90 @@ const $ = document.querySelector("");
 const $game = document.querySelector("#theGame");
 const $start = document.querySelector("#startbtn");
 const $h2 = document.querySelector("h2");
+const $speed = document.querySelector("#speed");
+const $speedlog = document.querySelector("#speedlog");
+const $zoom = document.querySelector("#zoom");
+const $zoomlog = document.querySelector("#zoomlog");
 
 let gameOn = false;
 
-let cols = 100;
-document.documentElement.style.setProperty("--game-cols", 100);
-let rows = 40;
-document.documentElement.style.setProperty("--game-rows", 40);
+let cols = 80;
+document.documentElement.style.setProperty("--game-cols", 80);
+let rows = 30;
+document.documentElement.style.setProperty("--game-rows", 30);
 let width = parseInt($game.offsetWidth);
-//                                                               to fix
-//                                                                 ||     
-//                                                                 \/    
-document.documentElement.style.setProperty("--game-cell-size", "13.43px");
+let cellsize = width / cols - 1;
+
+let zoom = parseInt($zoom.value);
+let probaset = 4;
+
+let thegame = [];
+
+const resizeInter = setInterval(resize, 2500);
+
+document.documentElement.style.setProperty("--game-cell-size", cellsize + "px");
 
 for (let i = 0; i < rows * cols; i++) {
-    const div = document.createElement("div");
-    const div2 = document.createElement("div");
-    div.classList.add("pixel");
-    if (Math.round(Math.random() * 3) == 0) {
-        div.classList.add("on");
-    }
-    div.appendChild(div2);
-    $game.appendChild(div);
+	let cell = createCell()[0];
+	$game.appendChild(cell);
 }
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    width = window.innerWidth;
-    console.log("js on");
-    console.log(width);
+	width = window.innerWidth;
 });
 
 $start.addEventListener("click", () => {
-    if (gameOn) {
-        $h2.textContent = "START";
-        gameOn = false;
-    } else {
-        $h2.textContent = "STOP/";
-        gameOn = true;
-    }
+	if (gameOn) {
+		$h2.textContent = "START";
+		gameOn = false;
+	} else {
+		$h2.textContent = "STOP/";
+		gameOn = true;
+	}
 });
+
+$speed.addEventListener("input", () => {
+	$speedlog.textContent = $speed.value;
+});
+
+$zoom.addEventListener("input", () => {
+	$zoomlog.textContent = $zoom.value;
+	zoom = parseInt($zoom.value);
+});
+
+function createCell() {
+	const div = document.createElement("div");
+	const div2 = document.createElement("div");
+
+	div.classList.add("pixel");
+
+	let islife = Math.floor(Math.random() * probaset) == 0 ? true : false;
+	islife ? div.classList.add("on") : div.classList.add("off");
+
+	div.appendChild(div2);
+
+	div.addEventListener("click", () => {
+		if (islife == true) {
+			div.classList.remove("on");
+			div.classList.add("off");
+			islife = false;
+		} else {
+			div.classList.remove("off");
+			div.classList.add("on");
+			islife = true;
+		}
+	});
+
+	return [div, islife == 0 ? true : false];
+}
+
+function resize() {
+	width = parseInt($game.offsetWidth);
+	cellsize = Math.floor(width / cols);
+	cellsize *= zoom;
+	document.documentElement.style.setProperty(
+		"--game-cell-size",
+		cellsize + "px",
+	);
+
+}
